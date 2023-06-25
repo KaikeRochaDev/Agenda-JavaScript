f742ae40f0c5f3d12b5b2dfa2b77a80b4addbffb
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-06-25 19:58:31 -0300

    Deleting useless stuff & changing controller name

--- routes.js
+++ routes.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
 
-
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
-
-route.get('/contato', contatoController.paginaInicial);
-
+route.get('/', homeController.index);
 
 module.exports = route;
