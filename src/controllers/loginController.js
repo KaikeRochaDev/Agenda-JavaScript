e6ca8dc994c117657547828ed3937b58857a4bf0
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-06-27 15:36:49 -0300

    Fixing bugs

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,31 +1,29 @@
-const Login = require('../models/LoginModel')
+const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login')
-}
+  res.render('login');
+};
 
-exports.register = async (req, res) => {
-    try{
-        const login = new Login(req.body)
-        await login.register()
-    
-        if(login.errors.length > 0){
-            req.flash('errors', login.errors)
-            req.session.save(function() {
-                return res.redirect("back")
-            })
-            return
-        }
+exports.register = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-        req.flash('success', "Seu usuário foi criado com sucesso.")
-        req.session.save(function() {
-            return res.redirect("back")
-        })
-    
-        return res.send(login.errors)
-
-    } catch(e) {
-        console.log(e)
-        return res.render('404')
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
     }
-}
+
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+    
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
